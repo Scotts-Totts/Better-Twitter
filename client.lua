009916f40fcb24c804eacf0e943c59f288a97c11
Scotts-Totts <69021545+Scotts-Totts@users.noreply.github.com>
2020-08-04 23:32:10 -0400

    Add files via upload
    fixed invalid name error

--- client.lua
+++ client.lua
@@ -64,5 +64,5 @@ RegisterCommand("sethandle", function(source,args) -- set twitter handle for atw
 end)
 
 RegisterCommand("twt", function(source, args) --normal tweet using player name as handle
-    TriggerServerEvent("tweet", "@"..GetPlayerName(source).."^0: "..table.concat(args," "))
+    TriggerServerEvent("tweet", "@"..GetPlayerName(PlayerId()).."^0: "..table.concat(args," "))
 end)
